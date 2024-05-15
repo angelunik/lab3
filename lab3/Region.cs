fd80e97737fcd56d472f94ee50c7a6c4015951bf
leratimofeeva <leratimofeeva129@gmail.com>
2024-05-15 20:04:32 +0300

    Добавление полей в класс регион

--- lab3/Region.cs
+++ lab3/Region.cs
@@ -8,6 +8,23 @@ namespace lab3
 {
     internal class Region
     {
-        private string name;
+        public string RegionName;
+        public double N2009;
+        public double N2010;
+        public double N2011;
+        public double N2012;
+        public double N2013;
+        public double N2014;
+        public double N2015;
+        public double N2016;
+        public double N2017;
+        public double N2018;
+        public double N2019;
+        public double N2020;
+        public double N2021;
+        public double N2022;
+        public double N2023;
+
+        public double Pizm;
     }
 }
